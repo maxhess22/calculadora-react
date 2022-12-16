9ba367f8dd98f06dc3629033e4a669586f002476
maxhess22 <ma.hessalvarez@duocuc.cl>
2022-12-16 11:13:44 -0400

    cambios de estilo en los botones

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Boton from './components/boton';
 import Pantalla from './components/pantalla';
 import {evaluate} from 'mathjs'
 
 
 
+
 function App() {
   let [valor, setValor]= useState([ ])
+
 
   
   
     return(
-      <div className="contenedor-calculadora">
-        <Pantalla valor={valor} />
-        <div className='contenedor'>
-          <Boton nombre="7" metodo={()=> setValor(valor+ "7")} />
-          <Boton nombre="8" metodo={()=> setValor(valor+ "8")} />
-          <Boton nombre="9" metodo={()=> setValor(valor+ "9")} />
-          <Boton nombre="*" metodo={()=> (setValor(valor+"*"))} />
+      <React.Fragment>
+        <h1 className="title">Calculadora react</h1>
+        <div className="contenedor-calculadora">
+          <Pantalla valor={valor} />
+          <div className='contenedor'>
+            <Boton nombre="7" metodo={()=> setValor(valor+ "7")} />
+            <Boton nombre="8" metodo={()=> setValor(valor+ "8")} />
+            <Boton nombre="9" metodo={()=> setValor(valor+ "9")} />
+            <Boton nombre="X" metodo={()=> (setValor(valor+"*"))} />
+          </div>
+          <div className='contenedor'>
+            <Boton nombre="4" metodo={()=> setValor(valor+ "4" )} />
+            <Boton nombre="5" metodo={()=> setValor(valor+ "5" )} />
+            <Boton nombre="6" metodo={()=> setValor(valor+ "6")} />
+            <Boton nombre="-" metodo={()=> ( setValor(valor+"-") )} />
+          </div>
+          <div className='contenedor'>
+            <Boton nombre="1" metodo={()=> setValor(valor+ "1")} />
+            <Boton nombre="2" metodo={()=> setValor(valor+ "2")} />
+            <Boton nombre="3" metodo={()=> setValor(valor+ "3" )} />
+            <Boton nombre="+" metodo={()=> setValor(valor+"+")}/>
+          </div> 
+          <div className='contenedor'>
+            <Boton nombre="0" metodo={()=> setValor(valor+ "0")} />
+            <Boton nombre="." metodo={()=> setValor(valor+".")} />
+            <Boton nombre="DEL" metodo={()=> setValor(" ")} />
+            <Boton nombre="/" metodo={()=> setValor(valor+ "/")} />
+          </div>
+          <div>
+          <Boton nombre="=" metodo={()=> setValor(evaluate(valor))} />
+          </div>
         </div>
-        <div className='contenedor'>
-          <Boton nombre="4" metodo={()=> setValor(valor+ "4" )} />
-          <Boton nombre="5" metodo={()=> setValor(valor+ "5" )} />
-          <Boton nombre="6" metodo={()=> setValor(valor+ "6")} />
-          <Boton nombre="-" metodo={()=> ( setValor(valor+"-") )} />
-        </div>
-        <div className='contenedor'>
-          <Boton nombre="1" metodo={()=> setValor(valor+ "1")} />
-          <Boton nombre="2" metodo={()=> setValor(valor+ "2")} />
-          <Boton nombre="3" metodo={()=> setValor(valor+ "3" )} />
-          <Boton nombre="+" metodo={()=> setValor(valor+"+")}/>
-        </div> 
-        <div className='contenedor'>
-          <Boton nombre="0" metodo={()=> setValor(valor+ "0")} />
-          <Boton nombre="." metodo={()=> setValor(valor+".")} />
-          <Boton nombre="AC" metodo={()=> setValor(" ")} />
-          <Boton nombre="/" metodo={()=> setValor(valor+ "/")} />
-        </div>
-        <div>
-        <Boton nombre="=" metodo={()=> setValor(evaluate(valor))} />
-        </div>
-      </div>
+      </React.Fragment>
     );
   }
 
